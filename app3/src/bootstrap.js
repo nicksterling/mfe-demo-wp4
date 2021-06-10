5d47c5bcbd14ad7c9a8ed35618251da458a4e89f
Nick Sterling <nicholas.v.sterling@gmail.com>
2021-06-10 12:15:39 -0600

    Added some polish

--- app3/src/bootstrap.js
+++ app3/src/bootstrap.js
@@ -1,1 +1,16 @@
-//Start bootstrapping your app here
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import { registerApplication, start } from 'single-spa';
+
+registerApplication({
+    name: 'app',
+    app: () => import("./App.Singlespa"),
+    activeWhen: ['/']
+})
+
+start()
